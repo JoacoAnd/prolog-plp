e66c2016bd6effa17a0b0115428552036dc52f8d
JoacoAnd <joaquinandreone@hotmail.com>
2025-06-11 12:17:02 -0300

    1 y 2

--- katamino.pl
+++ katamino.pl
@@ -1,3 +1,14 @@
 :- use_module(piezas).
 
-% Completar ...
+% 1- sublista(+Descartar, +Tomar, +L, -R)
+
+sublista(Descartar, Tomar, L, R) :- append(DL, RyAlgoMas, L), length(DL, Descartar), append(R, _, RyAlgoMas), length(R, Tomar).
+
+% 2- tablero(+K, -T)
+
+copiar(_, 0, []).
+copiar(L, N, [X|XS]) :-
+    X is L,
+    N1 is N - 1,
+    copiar(L, N1, XS).
+tablero(K, T) :- K > 0, length(L, K), copiar(L, K, T).
