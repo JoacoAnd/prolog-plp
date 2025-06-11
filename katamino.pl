573f61ce031576dac711a91191b795bf07fda8e4
JoacoAnd <joaquinandreone@hotmail.com>
2025-06-11 18:21:42 -0300

    ej 3 a 5

--- katamino.pl
+++ katamino.pl
@@ -5,10 +5,25 @@
 sublista(Descartar, Tomar, L, R) :- append(DL, RyAlgoMas, L), length(DL, Descartar), append(R, _, RyAlgoMas), length(R, Tomar).
 
 % 2- tablero(+K, -T)
+fila(K, Fila) :- length(Fila, K).
+tablero(K, T) :- K > 0, length(T, 5), length(L, K), maplist(fila(K), T).
 
-copiar(_, 0, []).
-copiar(L, N, [X|XS]) :-
-    X is L,
-    N1 is N - 1,
-    copiar(L, N1, XS).
-tablero(K, T) :- K > 0, length(L, K), copiar(L, K, T).
+% 3- tamaño(+M, -F, -C)
+
+tamanio(Matriz, Filas, Columnas) :- length(Matriz, Filas), member(Fila, Matriz), length(Fila, Columnas).
+
+% 4- coordenadas(+T, -IJ)
+
+coordenadas(T, (I, J)) :- tamanio(T, Filas, Columnas), between(1, Filas, I), between(1, Columnas, J).
+
+% 5- kPiezas(+K, -PS)
+
+elegirPieza([P | Piezas], P, Piezas).
+elegirPieza([_ | Piezas], P, Resto) :- elegirPieza(Piezas, P, Resto).
+
+generarLista(0, _, []).
+generarLista(K, Piezas, [P | PS]) :- K > 0, elegirPieza(Piezas, P, RestoPiezas), K1 is K - 1, generarLista(K1, RestoPiezas, PS).
+
+kPiezas(K, PS) :- nombrePiezas(Piezas), generarLista(K, Piezas, PS).
+
+% 6-
